751c60fcdbb9ed5afe816fde510575769e284026
Eduard Popescu <eduard.popescu@ness.com>
2019-09-06 11:20:55 +0300

    Moved DTO types closer to usage location.

--- NESS.VoucherManagement.Application/IHolidayProvider.cs
+++ NESS.VoucherManagement.Application/IHolidayProvider.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace NESS.VoucherManagement.Application
+﻿namespace NESS.VoucherManagement.Application
 {
-	public interface IHolidayProvider {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+
+	public interface IHolidayProvider
+	{
 		IEnumerable<DateTime> GetHolidays(int year, int month);
 	}
 }
--- NESS.VoucherManagement/ViewModels/WebServiceHolidayProvider.cs
+++ NESS.VoucherManagement/ViewModels/WebServiceHolidayProvider.cs
@@ -1,13 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
-using Newtonsoft.Json;
-using NESS.VoucherManagement.Application;
-using NESS.VoucherManagement.Core;
 
 namespace NESS.VoucherManagement.ViewModels
 {
+	using System.Collections.Generic;
+	using System.Net.Http;
+	using Application;
+	using Newtonsoft.Json;
+
 	internal class WebServiceHolidayProvider : IHolidayProvider
 	{
 		public IEnumerable<DateTime> GetHolidays(int year, int month)
@@ -15,10 +15,37 @@ namespace NESS.VoucherManagement.ViewModels
 			var calendar = new HttpClient().GetStringAsync($"http://kayaposoft.com/enrico/json/v1.0/?action=getPublicHolidaysForYear&year={year}&country=rou").Result;
 
 			return JsonConvert.DeserializeObject<BankHolidayResponseDto[]>(calendar)
-			                  .Select(x => new DateTime(x.BankHolidayDateResponseDto.Year, x.BankHolidayDateResponseDto.Month,
-				                          x.BankHolidayDateResponseDto.Day))
-			                  .Where(x => x.Month == month)
-			                  .ToList();
+				.Select(x => new DateTime(x.BankHolidayDateResponseDto.Year, x.BankHolidayDateResponseDto.Month,
+					x.BankHolidayDateResponseDto.Day))
+				.Where(x => x.Month == month)
+				.ToList();
+		}
+
+		internal class BankHolidayResponseDto
+		{
+			[JsonProperty("date")]
+			public BankHolidayDateResponseDto BankHolidayDateResponseDto { get; set; }
+
+			[JsonProperty("localName")]
+			public string LocalName { get; set; }
+
+			[JsonProperty("englishName")]
+			public string EnglishName { get; set; }
+		}
+
+		internal class BankHolidayDateResponseDto
+		{
+			[JsonProperty("day")]
+			public int Day { get; set; }
+
+			[JsonProperty("month")]
+			public int Month { get; set; }
+
+			[JsonProperty("year")]
+			public int Year { get; set; }
+
+			[JsonProperty("dayOfWeek")]
+			public int DayOfWeek { get; set; }
 		}
 	}
 }
